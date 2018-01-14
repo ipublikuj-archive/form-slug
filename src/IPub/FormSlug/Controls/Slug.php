e134cf09a7bd9576e1773cebaace56a8ebb65566
Adam Kadlec <adam.kadlec@fastybird.com>
2018-01-14 18:58:14 +0100

    - Added support for disabling field change

--- src/IPub/FormSlug/Controls/Slug.php
+++ src/IPub/FormSlug/Controls/Slug.php
@@ -76,6 +76,13 @@ class Slug extends Forms\Controls\TextInput
 	private $onetimeAutoUpdate = TRUE;
 
 	/**
+	 * Enable or disable updating field when editing watched field
+	 *
+	 * @var bool
+	 */
+	private $forceEditUpdate = FALSE;
+
+	/**
 	 * @param UI\ITemplateFactory $templateFactory
 	 * @param string|NULL $label
 	 * @param int|NULL $maxLength
@@ -98,6 +105,46 @@ class Slug extends Forms\Controls\TextInput
 	{
 		// Assign filed to collection
 		$this->fields[$field->getHtmlId()] = $field;
+
+		return $this;
+	}
+
+	/**
+	 * @return self
+	 */
+	public function disableOneTimeUpdate() : self
+	{
+		$this->onetimeAutoUpdate = FALSE;
+
+		return $this;
+	}
+
+	/**
+	 * @return self
+	 */
+	public function enableOneTimeUpdate() : self
+	{
+		$this->onetimeAutoUpdate = TRUE;
+
+		return $this;
+	}
+
+	/**
+	 * @return self
+	 */
+	public function disableForceEditUpdate() : self
+	{
+		$this->forceEditUpdate = FALSE;
+
+		return $this;
+	}
+
+	/**
+	 * @return self
+	 */
+	public function enableForceEditUpdate() : self
+	{
+		$this->forceEditUpdate = TRUE;
 
 		return $this;
 	}
@@ -131,9 +178,10 @@ class Slug extends Forms\Controls\TextInput
 
 		// Component js settings
 		$template->settings = [
-			'toggle'  => $this->toggleBox,
-			'onetime' => $this->onetimeAutoUpdate,
-			'fields'  => (array_reduce($this->fields, function (array $result, Forms\Controls\BaseControl $row) {
+			'toggle'    => $this->toggleBox,
+			'onetime'   => $this->onetimeAutoUpdate,
+			'forceEdit' => $this->forceEditUpdate,
+			'fields'    => (array_reduce($this->fields, function (array $result, Forms\Controls\BaseControl $row) {
 				$result[] = '#' . $row->getHtmlId();
 
 				return $result;
